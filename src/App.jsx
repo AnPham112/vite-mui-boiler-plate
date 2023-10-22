416ee65b99f91e621240cb9d0df60f7940d4580a
AnPham <anpham1122000@gmail.com>
2023-10-22 16:52:09 +0700

    init code

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Button from '@mui/material/Button'
+
+function App() {
+
+  return (
+    <>
+      <p>Material UI</p>
+      <Button variant='text'>text</Button>
+      <Button variant='contained'>contained</Button>
+      <Button variant='outlined'>outlined</Button>
+    </>
+  )
+}
+
+export default App
