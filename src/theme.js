f43c603aef371014fdc0ce18350c9982ba30ec0b
AnPham <anpham1122000@gmail.com>
2023-10-22 23:05:10 +0700

    update

--- src/theme.js
+++ src/theme.js
@@ -1,20 +1,19 @@
 import { experimental_extendTheme as extendTheme } from '@mui/material/styles'
+import { cyan, deepOrange, orange, teal } from '@mui/material/colors'
 
 // Create a theme instance.
 const theme = extendTheme({
   colorSchemes: {
     light: {
       palette: {
-        primary: {
-          main: '#ff5252'
-        }
+        primary: teal,
+        secondary: deepOrange
       }
     },
     dark: {
       palette: {
-        primary: {
-          main: '#000'
-        }
+        primary: cyan,
+        secondary: orange
       }
     }
   }
